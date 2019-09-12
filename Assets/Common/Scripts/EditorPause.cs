3177b2fc6b32d8b169edfd07d2766b73b1880293
iiporpammep <iiporpammep@mail.ru>
2019-09-12 13:58:34 +0300

    Editor Pause for debug

---
+++ Assets/Common/Scripts/EditorPause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace StoryProgramming
+{
+    public class EditorPause : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Debug.Break();
+            }
+        }
+    }
+
+}
